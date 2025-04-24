3f159a6245df3c06a266802e2a0cf826419921b8
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-24 14:41:29 -0400

    modify to include Redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 1️⃣ Import Provider and store
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* 2️⃣ Wrap App with Provider */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
