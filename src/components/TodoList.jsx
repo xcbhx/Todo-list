93c1ec16a3650d64e0bbdd7cfacc75ac5e8d4cf8
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-24 15:05:26 -0400

    update to apply styles dynamically and properly handle deletions

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleComplete, deleteTodo } from "../redux/todoSlice";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+  const [removingId, setRemovingId] = useState(null);
+
+  const handleDelete = (id) => {
+    setRemovingId(id);
+    setTimeout(() => {
+      dispatch(deleteTodo(id));
+      setRemovingId(null);
+    }, 300);
+  };
 
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>
+        <li
+          key={todo.id}
+          className={`${todo.completed ? "completed" : ""} ${removingId === todo.id ? "removed" : ""}`}
+        >
           <input
             type="checkbox"
             checked={todo.completed}
             onChange={() => dispatch(toggleComplete(todo.id))}
           />
           {todo.text}
-          <button onClick={() => dispatch(deleteTodo(todo.id))}>❌</button>
+          <button onClick={() => handleDelete(todo.id)}>❌</button>
         </li>
       ))}
     </ul>
