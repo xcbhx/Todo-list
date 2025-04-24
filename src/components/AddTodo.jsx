4e680b74a9315a8d793b88ac0c36c577692ebf8c
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-24 14:52:05 -0400

    displatching actions with useDispatch

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todoSlice";
+
+const AddTodo = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim()) {
+      dispatch(addTodo(text));
+      setText("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodo;
