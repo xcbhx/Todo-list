2db799f825b641d2998d103c4a9a8d0c66ca2fa1
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-24 15:22:35 -0400

    load saved state when app starts

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
+import { loadState, saveState } from "./localStorage";
+
+const preloadedState = loadState() || { todos: [] }; 
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  preloadedState, // This sets the initial state when the app starts
+});
+
+store.subscribe(() => {
+  saveState(store.getState().todos); // Still saving only the todos array
 });
