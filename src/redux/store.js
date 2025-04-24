2944cf4f7e0715eb41b47b050d7ec0253a5f07c9
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-24 15:31:31 -0400

    update store to include filters

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
+import filtersReducer from "./filtersSlice";
 import { loadState, saveState } from "./localStorage";
 
 const preloadedState = loadState() || { todos: [] }; 
@@ -7,10 +8,14 @@ const preloadedState = loadState() || { todos: [] };
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
+    filters: filtersReducer,
   },
   preloadedState, // This sets the initial state when the app starts
 });
 
 store.subscribe(() => {
-  saveState(store.getState().todos); // Still saving only the todos array
+  saveState({
+    todos: store.getState().todos,
+    filters: store.getState().filters,
+  });
 });
